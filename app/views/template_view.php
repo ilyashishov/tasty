8bee0d343f0898cb1639c7a34dda675980fa2a3a
ilya-shishov <stalk1258@gmail.com>
2015-11-23 18:53:16 +0300

    $1

--- app/views/template_view.php
+++ app/views/template_view.php
@@ -92,7 +92,7 @@
     <div style="clear: both"></div>
     <?php 
     if($url[1] == 'menu'){
-          echo '<a class="a-admin-page" href="/menu/new/id/<?php echo $url[3] ?>" >Добавить товар</a>';
+          echo '<a class="a-admin-page" href="/menu/new/id/'.$url[3].'" >Добавить товар</a>';
           echo '<a class="a-admin-page" href="/menu/new_category">Добавить категорию</a>';
         }
      ?>
