41f433f241ba7d450094d21bbd81adcaca8a610a
ilya-shishov <stalk1258@gmail.com>
2015-11-23 19:02:35 +0300

    $1

--- app/bl/MenuWork.php
+++ app/bl/MenuWork.php
@@ -15,7 +15,7 @@ class MenuWork
 
     public function CreateGoods($data){
         $conn = new DbConnector();
-        $query = "INSERT INTO goods (\"name\",\"id_cat\",\"desc\",\"m_desc\",\"img\",\"m_img\",\"weight\",\"price\") VALUES('$data[name]','$data[id_cat]','$data[desc],'$data[m_desc],'$data[img],'$data[m_img],'$data[weight],'$data[price])";
+        $query = "INSERT INTO goods (\"name\",\"id_cat\",\"desc\",\"m_desc\",\"img\",\"m_img\",\"weight\",\"price\") VALUES('$data[name]','$data[id_cat]','$data[desc]','$data[m_desc]','$data[img]','$data[m_img]','$data[weight]','$data[price]')";
         $conn->select($query);
         print_r($conn->error());
     }
