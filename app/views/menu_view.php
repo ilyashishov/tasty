81c18692252339c7d07b4407afa73ef3731a56c6
ilya-shishov <stalk1258@gmail.com>
2015-11-24 05:24:01 +0300

    $1

--- app/views/menu_view.php
+++ app/views/menu_view.php
@@ -19,6 +19,18 @@
 			foreach ($data['goods'][0] as $key) {
 				$edit = '<a  href="/menu/edit/id/'.$key["id"].'" class="lsf symbol edit-icon" style="font-size: 30px;">edit</a>';
 				$delete = '<a  href="/menu/delete/id/'.$key["id"].'" class="lsf symbol delete-icon" style="font-size: 30px;">delete</a>';
+				$buy = 0;
+				$i = 0;
+				foreach ($variable as $key2 => $value2) {
+					if($key['id'] == $value2 && $buy == 0){
+						$buy = 1;
+					}else{
+						$buy = 0;
+					}
+					if($key['id'] == $value2) $i++;
+
+				}
+				if(!$i)$ = 1;
 				printf('
 						<div class="block">
 							'.$edit.'
@@ -29,7 +41,7 @@
 							<img src="%s" alt="">
 							<div class="number" id="number%s" style="display: none;">
 								<!-- value="<?php echo $prow->quantity ?>" --> 
-								<input type="text" title="" class="inputbox input-ultra-mini" size="3" maxlength="4" name="quantity"  value="1" />
+								<input type="text" title="" class="inputbox input-ultra-mini" size="3" maxlength="4" name="quantity"  value="'.$i.'" />
 								<a href="#" id_good="%s" price="%s"  class="plus" style="left:108px;top:49px" ><img src="/img/plus.png" style="width:14px;height:14px;" alt=""></a>
 								<a href="#" id_good="%s" price="%s" class="minus"  style="left:35px;top:53px" ><img src="/img/minus.png" style="width:15px;height:5px;" alt=""></a>
 							</div>
@@ -38,7 +50,7 @@
 								<p class="name">%s</p>
 								<p class="m_desc">%s</p>
 								<a href=""><div class="buy"></div></a>
-								<a id="%s" buy="0" href="#" price="%s" class="add_good" ><div class="buy"></div></a>
+								<a id="%s" buy="'.$buy.'" href="#" price="%s" class="add_good" ><div class="buy"></div></a>
 							</div>
 						</div>
 						',$key["m_img"],$key["name"],$key["m_desc"],$key["weight"],$key["img"],$key["id"],$key["id"],$key["price"],$key["id"],$key["price"],$key["price"],$key["weight"],$key["name"],$key["desc"],$key["id"],$key["price"]);
