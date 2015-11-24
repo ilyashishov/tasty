a3f5a1efb47296bfc97ecde2e51d3f62b36828f9
ilya-shishov <stalk1258@gmail.com>
2015-11-24 04:44:28 +0300

    $1

--- app/core/model.php
+++ app/core/model.php
@@ -4,19 +4,10 @@ class Model {
     protected $_data;
 
     public function __constract($data){
-//        if(!isset($_COOKIE['currentCityId'])){
-//            setCookie("currentCityId", "25",time()+(3600*24*30), "/");
-//        }
-//        if(!isset($_COOKIE['currentCity'])){
-//            setCookie("currentCity", "Москва",time()+(3600*24*30), "/");
-//        }
-//        if(!isset($_COOKIE['currentCityName'])){
-//            setCookie("currentCityName", "moskva",time()+(3600*24*30), "/");
-//        }
         $this->_data = $data;
+        $this->_data = $data['basket'] = 99999;
     }
     public function get_data() {
-    	$data['basket'] = 99999;
-    	return $data;
+    	
     }
 }
