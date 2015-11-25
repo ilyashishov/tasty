7458c866f57e1d33ab5fa51c0a5689e7b0eba234
Ilya <79603278712@yandex.ru>
2015-11-25 16:09:54 +0300

    'commit'

--- app/views/basket_view.php
+++ app/views/basket_view.php
@@ -1,12 +1,13 @@
-<div class="cont3">
-	<div class="basket_active" id="basket_active" style="">
-		<p class="basket_t">Просмотр Вашего текущего заказа</p>
-		<a href="<?php print $_SERVER['HTTP_REFERER']; ?>" class="c">
-			<img src="./img/close.png" alt="">
-			<p>Свернуть</p>
-		</a>
-	
-	
+<div class="basket_active" id="basket_active" style="">
+	<p class="basket_t">Просмотр Вашего текущего заказа</p>
+	<a href="<?php print $_SERVER['HTTP_REFERER']; ?>" class="c">
+		<img src="./img/close.png" alt="">
+		<p>Свернуть</p>
+	</a>
+	<div class="cont3">
+		
+	</div>
+	<div class="foot">
 		<center><a href="#" class="checkout_2" id="checkout_2" >Оформить доставку</a></center>
 		<p class="summ" id="sum">Итого <span>&nbsp;<?=$_SESSION['price']?></span> руб.</p>
 		<p class="proc2" >Скидка <span>&nbsp;<?=$_SESSION['shere']?></span>%</p>
