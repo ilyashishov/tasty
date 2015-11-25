2c2d95aa894b1a6e6e09dcd38a1467052cbae850
Ilya <79603278712@yandex.ru>
2015-11-25 13:33:34 +0300

    'commit'

--- app/models/model_share.php
+++ app/models/model_share.php
@@ -13,29 +13,41 @@ class Model_share extends Model{
     }
 
     public function set_shere($id){
-    	$allPrice = 0;
+        $allPrice = 0;
+    	$allPrice2 = 0;
     	$goods = new MenuWork();
     	$_SESSION['shere'] = $id;
     	foreach ($_SESSION['basket'] as $key => $value) {
         	$arr = $goods->Goods($value);
         	$allPrice += $arr[0][0]['price'];
     	}
+        foreach ($_SESSION['basket_share'] as $key => $value) {
+            $arr = $goods->Share($value);
+            $allPrice2 += $arr[0][0]['to_price'];
+        }
     	$allPriceShere = ($allPrice * $_SESSION['shere']) / 100;
     	$allPrice -= $allPriceShere;
+        $allPrice += $allPrice2;
     	$_SESSION['price'] = $allPrice;
     	return print_r($allPrice);
     }
 
     public function delete_shere($id){
-    	$allPrice = 0;
+        $allPrice = 0;
+    	$allPrice2 = 0;
     	$goods = new MenuWork();
     	$_SESSION['shere'] = 0;
     	foreach ($_SESSION['basket'] as $key => $value) {
         	$arr = $goods->Goods($value);
         	$allPrice += $arr[0][0]['price'];
     	}
+        foreach ($_SESSION['basket_share'] as $key => $value) {
+            $arr = $goods->Share($value);
+            $allPrice2 += $arr[0][0]['to_price'];
+        }
     	$allPriceShere = ($allPrice * $_SESSION['shere']) / 100;
     	$allPrice -= $allPriceShere;
+        $allPrice += $allPrice2;
     	$_SESSION['price'] = $allPrice;
     	return print_r($allPrice);
     }
