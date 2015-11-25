48883f363220c99db72fd4b491422812424be93e
ilya-shishov <stalk1258@gmail.com>
2015-11-25 22:37:31 +0300

    $1

--- app/views/noodles_view.php
+++ app/views/noodles_view.php
@@ -1,1 +1,59 @@
-noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles noodles 
+ <div class="cont" style="overflow: hidden;margin-left: 30px;position: relative;height:470px;">
+
+  <p class="menu_t" style="">собери сам всего за 200 рублей!
+  </p>
+
+
+
+  <form name="test" class="form_menu" >
+    <p style="float:left;margin-left:20px;margin-top:50px;" class="discount_cal"><b>Шаг 1:</b><Br>
+     <input class="step1" type="radio" name="step1" value="пшеничная лапша"  onchange="check()" checked>пшеничная лапша<Br>
+     <input class="step1" type="radio" name="step1" value="гречневая лапша"   onchange="check()">гречневая лапша<Br>
+     <input class="step1" type="radio" name="step1" value="яичная лапша"  onchange="check()">яичная лапша<Br>
+     <input class="step1" type="radio" name="step1" value="рисовая лапша"  onchange="check()">рисовая лапша<Br>
+     <input class="step1" type="radio" name="step1" value="рис"  onchange="check()">рис<Br>
+   </p>
+   <div style="clear: both"></div>
+
+   <p style="float:left;margin-left:20px;margin-top:50px;" class="discount_cal" ><b>Шаг 2:</b><Br>
+     <input   type ="checkbox" name="step2" value="говядина" onchange="check2()" >говядина<Br>
+     <input   type ="checkbox" name="step2" value="свинина" onchange="check2()">свинина<Br>
+     <input  type  ="checkbox" name="step2" value="курица" onchange="check2()">курица<Br>
+     <input   type ="checkbox" name="step2" value="креветки" onchange="check2()">креветки<Br>
+     <input   type ="checkbox" name="step2" value="лосось" onchange="check2()">лосось<Br>
+     <input  type  ="checkbox" name="step2" value="бекон" onchange="check2()">бекон<Br>
+   </p>
+
+
+   <img src="./img/menu/lapsha/lapshaimg.png">
+
+
+   <p style="float:right;margin-top:-105px;margin-right:5px;" class="discount_cal"><b>Шаг 3:</b><Br>
+     <input class="step3" type="checkbox" name="step3" value="сыр" onchange="check3()">сыр<Br>
+     <input class="step3" type="checkbox" name="step3" value="перец болгарский" onchange="check3()">перец болгарский<Br>
+     <input class="step3" type="checkbox" name="step3" value="помидоры" onchange="check3()">помидоры<Br>
+     <input class="step3" type="checkbox" name="step3" value="шампиньоны" onchange="check3()">шампиньоны<Br>
+     <input class="step3" type="checkbox" name="step3" value="стручковая фасоль" onchange="check3()">стручковая фасоль<Br>
+   </p>
+   <div style="clear: both"></div>
+   <p style="float:right;margin-top:-105px;margin-right:29px;" class="discount_cal"><b>Шаг 4:</b><Br>
+     <input class="step4" type="radio" name="step4" value="соус карри" onchange="check4()" checked>соус карри<Br>
+     <input class="step4" type="radio" name="step4" value="соус терияки" onchange="check4()">соус терияки<Br>
+     <input class="step4" type="radio" name="step4" value="соус кимчи" onchange="check4()">соус кимчи<Br>
+     <input class="step4" type="radio" name="step4" value="томатный соус" onchange="check4()">томатный соус<Br>
+     <input class="step4" type="radio" name="step4" value="сливочный соус" onchange="check4()">сливочный соус<Br>
+   </p>
+   <div style="clear: both"></div>
+
+
+ <!--  <p style="text-align:center;margin-top:60px;"><input type="submit" class="t" style="background:none;border:none;text-decoration:underline;" value="Добавить в корзину"></p>
+
+-->
+<center>
+  <p style="position: absolute;top:390px;color:#C9A956;left:70px;">дополнительно ингридиенты из шага 2 стоят 30 руб. из шага 3 стоят 15 руб.</p>
+</center>
+<a href="#" buy="0" class="ad" id="t" style="displey: block">Добавить в корзину</a></p>
+<a href="#" buy="1" class="ad" id="d" style="display :none">Убрать из корзины</a></p>
+</form>
+
+</div>
